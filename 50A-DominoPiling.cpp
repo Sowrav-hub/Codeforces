b5c1f833413f9277e9f90c68197b1de885d64742
Sowrav Bhowmik <sowravofficial02@gmail.com>
2025-07-14 23:43:46 +0600

    Add files via upload
    https://codeforces.com/problemset/problem/50/A

---
+++ 50A-DominoPiling.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//#include<cstdio>
+//#include<list>
+//#include<cmath>
+using namespace std;
+
+int main()
+{
+    int m, n, max;
+
+    cin >> m >> n;
+
+    max=(m*n)/2;
+
+    cout << max << endl;
+
+    return 0;
+}
